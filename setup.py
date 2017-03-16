534574d506283b2766e84d84ea252ec3e25552ef
Dominic Waithe <dominic_waithe@hotmail.com>
2017-03-16 11:33:29 +0000

    updating the setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import numpy
+from distutils.extension import Extension
+from Cython.Build import cythonize
+
+setup(name='nanosimpy',
+      version='0.0.1',
+      description='The nano simulation python libary',
+      url='https://github.com/dwaithe/nanosimpy',
+      author='Dominic Waithe',
+      author_email='dominic_waithe@hotmail.com',
+      license='MIT',
+      packages=['nanosimpy'],
+      
+      
+      include_dirs=[numpy.get_include()],
+      ext_modules =  cythonize("nanosimpy/*.pyx"),
+      install_requires=[
+          'numpy', 'matplotlib', 'scipy', 'tifffile','cython' 
+      ],
+      zip_safe=False)
