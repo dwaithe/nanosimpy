c8d9446312e3634f566f7c5e094a2df46870e26a
Dominic Waithe <dominic_waithe@hotmail.com>
2017-03-16 14:46:37 +0000

    nanosimpy test

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -17,6 +17,6 @@ done
 
 # Install packages and test
 for PYBIN in /opt/python/*/bin/; do
-    "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
-    (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
+    "${PYBIN}/pip" install nanosimpy --no-index -f /io/wheelhouse
+    #(cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
 done
