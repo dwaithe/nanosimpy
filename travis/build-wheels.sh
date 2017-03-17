a97c8ace9c227d8d5f7a203409c12b9529c6b7d5
Dominic Waithe <dominic_waithe@hotmail.com>
2017-03-17 10:29:46 +0000

    secure

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -3,6 +3,11 @@ set -e -x
 
 # Install a system package required by our library
 
+for PYBIN in /opt/python/cp26-cp26mu/bin; do
+    "${PYBIN}/pip" install -r /io/dev-requirements.txt
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
+
 
 # Compile wheels
 for PYBIN in /opt/python/cp27-cp27mu/bin; do
